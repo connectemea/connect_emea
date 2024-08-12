49bdd327917ee7a91035a048d8881f9d5fc93efc
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-08-13 01:10:09 +0530

    feat(layout): add new Footer component to layout

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Logo from '../../assets/icons/FooterLogo.png'
+import { Instagram, Linkedin, Github, InstagramIcon } from "lucide-react";
+import socialLink from '../../const/socialLinks';
+import navLinks from '../../const/navLinks';
+
+function Footer() {
+    return (
+        <footer className='bg-black text-white flex flex-col gap-10 p-8 rounded-t-[50px]'>
+            <h1 className='mx-auto text-2xl font-semibold'>
+                <span className='text-orange-500 mr-1'>Join us</span>
+                to start your journey.
+            </h1>
+            <div className='flex justify-between'>
+                <div className='flex flex-col'>
+                    <h1>ConnectEMEA</h1>
+                    {navLinks.map((link, index) => (
+                        <div key={index} className="flex items-start justify-start gap-4">
+                            <a href={link.href} >{link.label}</a>
+                        </div>
+                    ))}
+                </div>
+
+                <div className='flex flex-col gap-4'>
+                    <img src={Logo} alt='logo' className='h-10' />
+                    <div className="flex items-end justify-end gap-4">
+                        {socialLink.map((item, index) => (
+                            <div key={index}>
+                                <a href={item.href} target="_blank" rel="noopener noreferrer">
+                                    <item.Icon className="w-5 h-5" />
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
