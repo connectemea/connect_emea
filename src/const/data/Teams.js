4dff0e4792f08ec54c2ec860462fde6f2d41acbb
shifna shirin <shifnashirin93@gmail.com>
2024-08-12 16:48:12 +0530

    data added

--- src/const/data/Teams.js
+++ src/const/data/Teams.js
@@ -1,5 +1,5 @@
 import { Afeef, Aseel, Nahyan, Salman } from '../../assets/images/founders'
-import { Fabin, Saleel, Shamil } from '../../assets/images/interns'
+import { Amjad, Athif, Dayyan, Fabin, Gayathri, Hanana, Jannah, Marva, Rafeeda, Rashid, Rizwan, Saleel, Shahbana, Shamil, Shifna, Sibna, Sunain } from '../../assets/images/interns'
 
 
 const FoundersData = [
@@ -59,7 +59,7 @@ const InternsData = [
         role: 'Graphic Designer',
         image: Saleel,
         social: {
-            linkedin: 'https://www.linkedin.com/',
+            linkedin: 'https://www.linkedin.com/in/muhammed-saleel-cp-84064524b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
             github: 'https://www.github.com/',
             instagram: 'https://www.instagram.com/'
         },
@@ -70,7 +70,7 @@ const InternsData = [
         role: 'Developer',
         image: Shamil,
         social: {
-            linkedin: 'https://www.linkedin.com/',
+            linkedin: 'https://www.linkedin.com/in/shamil-kp-65878227a',
             github: 'https://www.github.com/',
             instagram: 'https://www.instagram.com/'
         },
@@ -81,11 +81,169 @@ const InternsData = [
         role: 'Community Manager',
         image: Fabin,
         social: {
-            linkedin: 'https://www.linkedin.com/',
+            linkedin: 'https://www.linkedin.com/in/fabin-fasif-0554042b7/',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 4,
+        name: 'Sibna shirin',
+        role: 'Content writer',
+        image: Sibna,
+        social: {
+            linkedin: 'http://www.linkedin.com/in/sibna-sherin',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 5,
+        name: 'Muhammed Rashid',
+        role: 'Community Manager',
+        image: Rashid,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/muhammed-rashid-1044a8221?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 6,
+        name: 'Shifna Shirin',
+        role: 'Community Manager',
+        image: Shifna,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/shifna-shirin-9654b2255?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 7,
+        name: 'Fathima Jannah',
+        role: 'videographer',
+        image: Jannah,
+        social: {
+            linkedin: 'http://linkedin.com/in/fathima-jannah-88533a25b',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 8,
+        name: 'Athif Noor',
+        role: 'Community Manager',
+        image: Athif,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/athif-noor-ap-a879b4256?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 9,
+        name: 'Amjad',
+        role: 'Community Manager',
+        image: Amjad,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/amjxd-aj?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 10,
+        name: 'Fathima Rafeeda',
+        role: 'Community Manager',
+        image: Rafeeda,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/fathima-rafeeda-a-87079023b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 11,
+        name: 'Dayyan Ali',
+        role: 'Graphic Designer',
+        image: Dayyan,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/dayyan-ali?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 12,
+        name: 'Muhammed Sunain',
+        role: 'Community Manager',
+        image: Sunain,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/muhammed-sunain-287674259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 13,
+        name: 'Maryam Hanana',
+        role: 'Content Writer',
+        image: Hanana,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/mariyam-hanana-v-526a032b6/',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 14,
+        name: 'Fathima Marva',
+        role: 'Community Manager',
+        image: Marva,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/marva-kt-4378a925b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 15,
+        name: 'Gayathri',
+        role: 'Community Manager',
+        image: Gayathri,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/gayathri-p-95a63a261?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 16,
+        name: 'Rizwan',
+        role: 'Graphic Designer',
+        image: Rizwan,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/mohammed-rizwan-70662a232/',
+            github: 'https://www.github.com/',
+            instagram: 'https://www.instagram.com/'
+        },
+    },
+    {
+        id: 17,
+        name: 'Shahbana Backer',
+        role: 'Community Manager',
+        image: Shahbana,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/shahbana-backer-5537b5293/',
             github: 'https://www.github.com/',
             instagram: 'https://www.instagram.com/'
         }
     }
+    
+
+
+
 ]
 
 
--- src/pages/Team/components/Interns/index.jsx
+++ src/pages/Team/components/Interns/index.jsx
@@ -5,7 +5,7 @@ import  TeamsData  from '../../../../const/data/Teams';
 function Interns({}) {
   
     return (
-        <div className='flex items-start justify-center p-4 gap-8'>
+        <div className='flex items-start justify-center p-4 gap-8 flex-wrap'>
             {TeamsData.InternsData.map((intern, index) => (
                 <TeamCard key={index} data={intern} id={intern.id} size={'small'} />
             ))}
