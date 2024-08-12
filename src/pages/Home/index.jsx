2cd48311a885bd537dfd20a44227bdc4a3c65971
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-08-13 01:09:44 +0530

    fix(home): resolve rendering issues in the home page index

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,24 @@
+import Events from './components/Events';
+import Points from './components/Points';
+import About from './components/about';
+import Contact from './components/contact';
 import Section1 from './components/section1';
+import Section2 from './components/section2';
+import Section3 from './components/section3';
+import Welcome from './components/welcome';
 
 
 const Home = () => {
   return (
-    <div>
-     <Section1 />
+    <div className='min-h-[70vh]'>
+      <Welcome />
+      <Section1 />
+      <Points />
+      <Section2 />
+      <Events />
+      <Section3 />
+      <About />
+      <Contact />
     </div>
   );
 };
