15a0ef2095639cd3f1c2c9ddb0788a8a70ff7ec3
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-18 21:54:25 +0530

    chore: compress images for improved performance

--- src/pages/Home/components/Welcome/index.jsx
+++ src/pages/Home/components/Welcome/index.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Icon from '@/assets/avatars/welcome.png'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import Icon from '@/assets/avatars/welcome.png';
+import { useNavigate } from 'react-router-dom';
 
 function Welcome() {
+    const [loaded, setLoaded] = useState(false);
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
     const handleClick = () => {
-        navigate('/join')
-    }
+        navigate('/join');
+    };
 
     return (
         <section className='text-center flex flex-col gap-4 my-4'>
@@ -16,15 +18,32 @@ function Welcome() {
                     WHERE&nbsp; <span className='text-orange-500'>STUDENTS&nbsp; </span>TURN&nbsp; THEORY&nbsp;
                     <br />INTO&nbsp; PRACTICE&nbsp;
                 </h1>
-                <p className='text-sm sm:text-lg'>For students , by students.</p>
+                <p className='text-sm sm:text-lg'>For students, by students.</p>
             </div>
 
-            <button className='px-10 sm:px-14 py-1 sm:py-1.5 bg-orange-600 text-white rounded-full w-fit mx-auto font-bold tracking-normal transition-all hover:bg-orange-500' onClick={handleClick}>
-               Intens Register 
+            <button
+                className='px-10 sm:px-14 py-1 sm:py-1.5 bg-orange-600 text-white rounded-full w-fit mx-auto font-bold tracking-normal transition-all hover:bg-orange-500'
+                onClick={handleClick}
+            >
+                Intens Register
             </button>
-            <img src={Icon} alt='avatar' className='w-full max-w-[700px] h-auto mx-auto mt-6' />
+
+            {!loaded && (
+                <div className='absolute inset-0 bg-white flex items-center justify-center'>
+                    <p className='text-gray-500'>Loading...</p>
+                </div>
+            )}
+
+            <div className='relative'>
+                <img
+                    src={Icon}
+                    alt='avatar'
+                    className='w-full max-w-[700px] h-auto mx-auto mt-6'
+                    onLoad={() => setLoaded(true)}
+                />
+            </div>
         </section>
-    )
+    );
 }
 
-export default Welcome
+export default Welcome;
