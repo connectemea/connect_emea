7e4dfd44ff1d15c3c0a76393f1178e8117298491
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-18 17:26:36 +0530

    fix: update contact and join forms

--- src/pages/Home/components/Contact/Form.jsx
+++ src/pages/Home/components/Contact/Form.jsx
@@ -1,8 +1,8 @@
-"use client";
-
+import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Loader } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -16,61 +16,93 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Rocket } from "lucide-react";
+import { createRecord } from "@/utils/airtableService";
+import StatusModal from "@/components/common/Modal";
 
 const formSchema = z.object({
   email: z.string().email({
     message: "Invalid email address.",
   }),
-  quesries: z.string().min(2, {
-    message: "Quesries must be at least 2 characters.",
+  queries: z.string().min(5, {
+    message: "Quesries must be at least 5 characters.",
   }),
 });
 
 export function QueriesForm() {
+  const [modalStatus, setModalStatus] = useState('error'); // 'success' | 'error' | null
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
-      quesries: "",
+      queries: "",
     },
   });
 
-  function onSubmit(values) {
-    console.log(values);
+  async function onSubmit(values) {
+    try {
+      setLoading(true);
+      console.log(values);
+      await createRecord("Queries_table", values, 2);
+      setModalStatus('success');
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      setModalStatus('error');
+    } finally {
+      setLoading(false);
+      form.reset();
+      setModalOpen(true);
+    }
   }
-
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="flex">Email</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter Your Email" {...field} />
-              </FormControl>
-              <FormMessage className="flex"/>
-            </FormItem>
-          )}
-        />
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex">Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Email" {...field} />
+                </FormControl>
+                <FormMessage className="flex" />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="quesries"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="flex">Quesries</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter Your Queries" {...field} />
-              </FormControl>
-              <FormMessage className="flex"/>
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="flex gap-2 mx-auto w-full bg-orange-500 font-semibold transition-all hover:bg-orange-600">Send Now <span className="rotate-45"><Rocket /></span></Button>
-      </form>
-    </Form>
+          <FormField
+            control={form.control}
+            name="queries"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex">Quesries</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Your Queries" {...field} />
+                </FormControl>
+                <FormMessage className="flex" />
+              </FormItem>
+            )}
+          />
+          <Button type="submit"
+            disabled={loading}
+            className="flex gap-2 mx-auto w-full bg-orange-500 font-semibold transition-all hover:bg-orange-600 disabled:bg-orange-400">
+            {loading ? <Loader className="animate-spin" size={20} /> : <span className="flex gap-2 items-center justify-center">Send Now <Rocket className="rotate-45" size={20} /></span>}
+          </Button>
+        </form>
+      </Form>
+      <StatusModal
+        open={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        status={modalStatus}
+        title={modalStatus === 'success' ? 'Query Submitted Successfully' : 'Submission Error'}
+        description={modalStatus === 'success'
+          ? 'Thank you for your query. We will get back to you soon.'
+          : 'There was an error submitting your query. Please try again later.'}
+      />
+    </>
   );
 }
--- src/pages/Join/index.jsx
+++ src/pages/Join/index.jsx
@@ -5,10 +5,10 @@ import img from '../../assets/avatars/JoinNow.png'
 function Join() {
   return (
     <div className='p-3'>
-      <h1 className='font-bold py-10 flex justify-center text-2xl w-full'>Join page</h1>
+      <h1 className='font-bold py-10 flex justify-center text-2xl w-full'>Intern Hiring 2025 Registeration</h1>
       <div className='flex flex-col xl:flex-row bg-orange-500  mx-auto rounded-xl m-10 p-3 w-full  max-w-[800px] xl:max-w-[90%]'>
         <div className='flex items-center flex-col justify-center xl:w-[50%] xl:mx-auto'>
-          <h1 className='text-3xl font-bold text-white mx-auto mb-2'>Welcome 
+          <h1 className='text-3xl font-bold text-white mx-auto mb-2'>Welcome
             to Connect</h1>
           <img src={img} alt='join' className='w-auto ' />
         </div>
