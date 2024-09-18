a2b29cc9f1bbc6b2aa8f150d89bd43c454085187
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-18 23:34:03 +0530

    feat: enhance About page content and layout

--- src/pages/About/components/Vision/index.jsx
+++ src/pages/About/components/Vision/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import img from '../../../../assets/avatars/About2.png'
+import img from '@/assets/avatars/About2.png'
 
 function Vision() {
   return (
     <div className='flex flex-col-reverse md:flex-row justify-between mt-10 p-4'>
       <div className='md:w-1/2 flex items-start justify-center flex-col mx-auto'>
         <h1 className='text-left text-orange-500 font-black text-3xl mb-4'>Vision</h1>
-        <p>
-        We paint a larger picture of the student community to create a network with clusters of their area of interests and thus creating a potent workforce capable of unlocking the vast reservoir of untapped potential. By improving specific learning outcomes, it essentially places people first by empowering them with technical and creative skills to make sustainable choices and multi-dimensional solutions that the complex new world needs.
+        <p className='font-semibold text-lg md:text-xl'>
+          We envision a future where students use their college years not just for academic excellence but for life and career excellence. At Connect, we provide the resources and support to help every student become skilled, valuable, and capable of achieving their dreams—regardless of where they study.
         </p>
       </div>
       <div className='md:w-1/2 flex items-center justify-center mx-auto'>
