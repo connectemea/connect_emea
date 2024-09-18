274fcebbf7f5f9e7dc3aaa6ce34cfe30a8155726
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-19 00:23:20 +0530

    refactor(about): update mission and vision components

--- src/pages/About/components/Vision/index.jsx
+++ src/pages/About/components/Vision/index.jsx
@@ -5,7 +5,7 @@ function Vision() {
   return (
     <div className='flex flex-col-reverse md:flex-row justify-between mt-10 p-4'>
       <div className='md:w-1/2 flex items-start justify-center flex-col mx-auto'>
-        <h1 className='text-left text-orange-500 font-black text-3xl mb-4'>Vision</h1>
+        <h1 className='text-left mx-auto md:mx-0 text-orange-500 font-black text-3xl mb-4'>Vision</h1>
         <p className='font-semibold text-lg md:text-xl'>
           We envision a future where students use their college years not just for academic excellence but for life and career excellence. At Connect, we provide the resources and support to help every student become skilled, valuable, and capable of achieving their dreams—regardless of where they study.
         </p>
