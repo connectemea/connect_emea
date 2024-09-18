4449f497cea491a24b8265724e73f952ddbcb8dc
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-18 20:56:01 +0530

    style: update layout components

--- src/layout/GuestLayout.tsx
+++ src/layout/GuestLayout.tsx
@@ -1,16 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 import NavBar from "@/components/layout/NavBar";
 import Footer from "@/components/layout/Footer";
 
-
 function GuestLayout() {
-    return (
-        <div className='relative '>
-            <NavBar />
-            <Outlet />
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="relative flex flex-col w-full min-h-screen">
+      <NavBar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default GuestLayout
+export default GuestLayout;
