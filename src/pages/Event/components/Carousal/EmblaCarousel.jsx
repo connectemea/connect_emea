246a0539dc881c670bab73dc09d461d61acc0fa0
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-21 02:53:24 +0530

    fix: update EmblaCarousel for smoother animations

--- src/pages/Event/components/Carousal/EmblaCarousel.jsx
+++ src/pages/Event/components/Carousal/EmblaCarousel.jsx
@@ -16,7 +16,7 @@ const EmblaCarousel = (props) => {
     <section className="embla relative">
       <div className='relative w-full z-20 '>
         {!prevBtnDisabled && (<PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} className="z-20 absolute top-32 -left-10 max-h-40 max-w-40 cursor-pointer" />)}
-        {!nextBtnDisabled && (<NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} className="z-20 absolute top-32 right-[70px] md:-right-10 max-h-40 max-w-40 cursor-pointer" />)}
+        {!nextBtnDisabled && (<NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} className="z-20 absolute top-32 left-[250px] sm:left-[300px] md:left-[300px] max-h-40 max-w-40 cursor-pointer" />)}
       </div>
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
