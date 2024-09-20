3f9fac355b4bfa47082563e4a0c82b10b7bc964d
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-21 02:53:24 +0530

    refactor: improve Team page layout

--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -9,7 +9,7 @@ const Team = () => {
       <h1 className="font-semibold text-2xl flex items-center justify-center">Our Team</h1>
 
       <Founders InternsData={TeamsData.InternsData} />
-      <hr className="max-w-[600px] px-6 mx-auto bg-gray-400 h-[2px]" />
+      {/* <hr className="max-w-[600px] px-6 mx-auto bg-gray-400 h-[2px]" /> */}
       <div className="">
         <Interns FoundersData={TeamsData.FoundersData} />
       </div>
