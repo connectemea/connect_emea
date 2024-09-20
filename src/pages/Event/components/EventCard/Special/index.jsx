5c7d2698cfd45b16af6f8e24eff342f672a68854
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-21 02:53:24 +0530

    refactor: update Special EventCard for consistency

--- src/pages/Event/components/EventCard/Special/index.jsx
+++ src/pages/Event/components/EventCard/Special/index.jsx
@@ -10,17 +10,17 @@ function SpecialCard({ data }) {
         navigate('/event/' + data.id);
     }
     return (
-        <div className='p-4 min-w-[280px] max-w-[280px] space-y-2'>
+        <div className='sm:p-4 min-w-[180px] max-w-[240px] sm:max-w-[280px] space-y-2 cursor-pointer mx-auto' onClick={handleClick}>
             <div className='h-60 w-full bg-gray-300'>
                 <img src={`https://picsum.photos/600/350?v=${data.id}`} alt={data.title} className='w-full h-full object-cover' />
             </div>
-            <div className='text-[12px] flex justify-between'>
+            <div className='text-[12px] flex justify-between flex-wrap'>
                 <p>Date: {data.date}</p>
                 <p>Time: {data.time}</p>
             </div>
-            <h1 className='font-semibold'>{data.title}</h1>
+            <h1 className='font-semibold truncate'>{data.title}</h1>
             <div className='flex justify-end w-full'>
-                <button onClick={handleClick} className=' bg-orange-500 rounded-md px-2 uppercase flex gap-2 items-center text-[12px] justify-center font-semibold text-white'>open <ChevronsRight className='w-4' /></button>
+                <button onClick={handleClick} className=' bg-orange-500 rounded-md px-4 py-1 uppercase flex gap-2 items-center text-[12px] justify-center font-semibold text-white'>open <ChevronsRight className='w-4' /></button>
             </div>
         </div>
     )
