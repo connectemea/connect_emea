b2d66872cba50815ec3db24f43e768137644f3ed
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-27 20:51:07 +0530

    chore: add and update images in the Us directory

--- src/assets/images/Us/index.ts
+++ src/assets/images/Us/index.ts
@@ -1,5 +1,9 @@
 import BootCamp from './Bootcamb.jpeg';
 import inFront from './inFront.jpeg';
+import Image3 from './avt_2.jpeg';
+import Image4 from './bridge_c.jpeg';
+import Image5 from './iedc.jpeg';
+import Image6 from './seminar_s.jpeg';
+import Image7 from './shehike_2.jpeg';
 
-
-export { BootCamp, inFront };
+export { BootCamp, inFront, Image3, Image4, Image5, Image6, Image7 };
