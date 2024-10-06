a2d8d3ebb363107faabddf6a66c99d2271952547
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-10-06 12:34:41 +0530

    #

--- src/pages/Join/index.jsx
+++ src/pages/Join/index.jsx
@@ -6,12 +6,12 @@ import Closed from '@/assets/images/Us/closed_2.jpeg'
 function Join() {
   return (
     <div className='w-limit p-3'>
-       {/* <div className='max-w-4xl mx-auto p-6  rounded-lg mb-10'> */}
+       <div className='max-w-4xl mx-auto p-6  rounded-lg mb-10'>
       <h1 className='font-bold text-center text-3xl sm:text-4xl mb-6'>
         Intern Hiring Registration 2025
       </h1>
 
-      {/* <div className='relative'>
+      <div className='relative'>
         <img
           src={Closed}
           alt='closed'
@@ -41,19 +41,19 @@ function Join() {
         </a>
 
       </div>
-      </div> */}
-      <div className='flex flex-col lg:flex-row bg-gray-500  mx-auto rounded-xl my-8  p-3 w-full'>
-        <div className='flex items-center flex-col justify-center max-w-[500px] lg:min-w-[350px] xl:min-w-[450px] mx-auto'>
-          <h1 className='text-[48px]  text-white mx-auto  flex leading-[55px] flex-col items-center justify-center mb-10'>
-            <span>Welcome</span>
-            <span className='font-bold'>to Connect</span>
-          </h1>
-          <img src={img} alt='join' className='w-auto' />
-        </div>
-        <div className='bg-white w-full rounded-lg p-4 md:p-10 flex-grow mx-auto'>
-          <JoinForm />
-        </div>
       </div>
+      {/*  <div className='flex flex-col lg:flex-row bg-gray-500  mx-auto rounded-xl my-8  p-3 w-full'>
+      //   <div className='flex items-center flex-col justify-center max-w-[500px] lg:min-w-[350px] xl:min-w-[450px] mx-auto'>
+      //     <h1 className='text-[48px]  text-white mx-auto  flex leading-[55px] flex-col items-center justify-center mb-10'>
+      //       <span>Welcome</span>
+      //       <span className='font-bold'>to Connect</span>
+      //     </h1>
+      //     <img src={img} alt='join' className='w-auto' />
+      //   </div>
+      //   <div className='bg-white w-full rounded-lg p-4 md:p-10 flex-grow mx-auto'>
+      //     <JoinForm />
+      //   </div>
+       </div> */}
     </div>
   )
 }
